80a2a230bc4fb8b19fb9d9bd0905b4a3f581c6e4
Jaynish <jaynish@pyaxis.com>
2022-11-30 11:55:12 +0530

    Add project files.

---
+++ JwtWebApi.JwtCode/PassWordHash.cs
@@ -0,0 +1,58 @@
+﻿using JwtWebApi.Data.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace JwtWebApi.JwtCode
+{
+    public class PassWordHash
+    {
+        //private User user = new();
+
+        //public void inituser(User paramuser)
+        //{
+        //    user = paramuser;
+        //}
+        public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(SecurityKeys.Key));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+
+                claims: claims,
+                expires:DateTime.Now.AddDays(30),
+                signingCredentials:creds
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
+        public bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var comutedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return comutedHash.SequenceEqual(passwordHash);
+            }
+        }
+    }
+}
---
+++ JwtWebApi/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using JwtWebApi.Data.Models;
+using JwtWebApi.JwtCode;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new();
+        public PassWordHash PasswordHashClass = new();
+
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            PasswordHashClass.CreatePasswordHash(request.Password,out byte[] passwordHash,out byte[] passwordSalt);
+            user.Username= request.Username;
+            user.PasswordHash= passwordHash;
+            user.PasswordSalt= passwordSalt;
+            //PasswordHash.inituser(user);
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> Login(UserDto request)
+        {
+            if (request.Username != user.Username)
+            {
+                return BadRequest("User Not Found.");
+            }
+
+            var result = PasswordHashClass.VerifyPasswordHash(request.Password,user.PasswordHash,user.PasswordSalt);
+            if(!result)
+            {
+                return BadRequest("Wrong Password.");
+            }
+            string token = PasswordHashClass.CreateToken(user);
+            return Ok(token);
+        }
+    }
+}
