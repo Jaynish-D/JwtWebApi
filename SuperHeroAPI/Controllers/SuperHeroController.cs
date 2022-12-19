63e3ed9e07085140ace026f4de552ae26f745467
Jaynish <jaynish@pyaxis.com>
2022-12-19 18:21:06 +0530

    CURD Operation On SuperHero

--- SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -1,6 +1,8 @@
 ﻿using JwtWebApi.Data.Entites;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OnlineHotel.Repository.UnitOfWork2;
+using Microsoft.EntityFrameworkCore;
 
 namespace SuperHeroAPI.Controllers
 {
@@ -8,20 +10,56 @@ namespace SuperHeroAPI.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
+        private readonly IUnitofWork _uow;
+
+        public SuperHeroController(IUnitofWork uow)
+        {
+            _uow = uow;
+        }
+
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> GetAll()
         {
-            return new List<SuperHero>
+            return Ok( _uow.GenericRepository<SuperHero>().GetAll().ToList());
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> Create(SuperHero hero)
+        {
+            _uow.GenericRepository<SuperHero>().Add(hero);
+            _uow.Save();
+            return Ok(_uow.GenericRepository<SuperHero>().GetAll().ToList());
+        }
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> Update(SuperHero hero)
+        {
+            var dbhero = await _uow.GenericRepository<SuperHero>().GetByIdAsync(hero.Id);
+            if(dbhero == null)
             {
-                new SuperHero
-                {
-                    Id = 1,
-                    Name="SpiderMan",
-                    FirstName="Peter",
-                    LastName="Parker",
-                    Place = "New York City"
-                }
-            };
+                return NotFound("Hero Not Found");
+            }
+
+            dbhero.Name = hero.Name;
+            dbhero.FirstName = hero.FirstName;
+            dbhero.LastName = hero.LastName;
+            dbhero.Place = hero.Place;
+
+            _uow.Save();
+
+            return Ok(_uow.GenericRepository<SuperHero>().GetAll().ToList());
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> Delete(object id)
+        {
+            var dbHero = await _uow.GenericRepository<SuperHero>().GetByIdAsync(id);
+            if(dbHero == null)
+            {
+                return NotFound("Hero Not Found");
+            }
+
+            await _uow.GenericRepository<SuperHero>().DeleteAsync(dbHero);
+
+            _uow.Save();
+            return Ok(_uow.GenericRepository<SuperHero>().GetAll().ToList());
         }
     }
 }
